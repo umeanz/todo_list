fcb51b4b325f478b1dc5a96cdd8a928c8cb1a825
umeanz <umean201@gmail.com>
2022-06-15 20:20:30 -0500

    clear input

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const btn = document.getElementById('btn');
 
-
-
 btn.addEventListener('click', hello);
 
-function hello(){
-var item = document.getElementById('item').value;
- var li = document.createElement("li");
- var t = document.createTextNode(item); //html에 문자 생성 
- li.appendChild(t);
+function hello(e){
+  e.preventDefault();
+  var item = document.getElementById('item').value;
+  var li = document.createElement("li");
+  var t = document.createTextNode(item); //html에 문자 생성 
+  li.appendChild(t);
 
- if(item === ''){
-    alert("typing word");
- }else {   
-    document.getElementById("todolist").appendChild(li); 
- }
+  if(item === ''){
+   alert("typing word");
+  }else {
+   document.getElementById("todolist").appendChild(li); 
+   document.getElementById('item').value= " ";
+  }
 
 }
